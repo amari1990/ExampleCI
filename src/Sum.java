5bdd72f0697b22cf373e3150b3d1f0f8c7a327f8
amari1990 <amari@ilerna.com>
2024-11-18 14:08:04 +0100

    Setup CI

---
+++ src/Sum.java
@@ -0,0 +1,13 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+	 
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + sum);
+    }
+	
+
+}
